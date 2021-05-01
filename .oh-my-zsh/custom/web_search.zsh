8817983900ad6271d7154eafebfb00ffd1a0a201
valdeirpsr <valdeirpsr@hotmail.com.br>
2021-04-30 22:31:16 -0300

    Create web_search.zsh

---
+++ .oh-my-zsh/custom/web_search.zsh
@@ -0,0 +1,15 @@
+export ZSH_WEB_SEARCH_ENGINES=(
+    # DEV
+    stackoverflow_pt "https://pt.stackoverflow.com/search?q="
+    devto "https://dev.to/search?q="
+    mdn "https://developer.mozilla.org/en-US/search?q="
+    # POOTARIA =D
+    xvideos "https://www.xvideos.com/?k="
+    redtube "https://www.redtube.com.br/?search="
+    nudevista "https://www.nudevista.com.br/?q="
+    sex "https://www.sex.com/search/videos?query="
+    # Mídia
+    music "https://music.youtube.com/search?q="
+    spotify "https://open.spotify.com/search/"
+    soundcloud "https://soundcloud.com/search?q="
+)
