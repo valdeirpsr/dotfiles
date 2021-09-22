67a127ed107e6b90ec5a4f2f3e55a06ddd6187ae
valdeirpsr <valdeirpsr@hotmail.com.br>
2021-09-22 09:22:07 -0300

    feat: adiciona suporte a arquitetura arm64 para Docker

--- install.sh
+++ install.sh
@@ -63,9 +63,16 @@ function prepare_docker_install() {
         platform="ubuntu";
     fi
 
+    local arch="amd64";
+
+    # Raspberry
+    if [[ $(uname -p) =~ aarch64 ]]; then
+        arch="arm64";
+    fi
+
     if [[ $platform =~ ubuntu|debian ]]; then
         curl -fsSL "https://download.docker.com/linux/$platform/gpg" | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg;
-        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$platform $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+        echo "deb [arch=$arch signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$platform $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
         set +x;
     else
         echo "Não há suporte para $platform";
