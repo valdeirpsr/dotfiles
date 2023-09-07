5a9300dbe40774df3180a1117455277e30a04840
valdeirpsr <valdeirpsr@hotmail.com.br>
2023-09-07 11:38:01 -0300

    feat: adiciona custom-functions

---
+++ .custom-functions/watermark-south-west.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Usage
+#
+# One file
+# ./watermark-south.sh input-file.jpg
+#
+# Save in folder
+# ./watermark-south.sh input-file.jpg output-folder/
+#
+# Mass
+# for i in *.jpg; do ./watermark-south.sh "$i" output/; done
+#
+
+inputFile="$1"
+outputFolder="$2"
+outputFile="${inputFile%%.*}-watermark.${inputFile##*.}"
+width=$(identify -format "%w" $inputFile)
+height=$(identify -format "%h" $inputFile)
+
+pointSizeLarge=$(echo "$width / 18.75" | bc)
+
+if [ -z $outputFolder ]; then
+  outputFolder="$PWD"
+fi
+
+outputFolder=$(echo "$outputFolder" | sed 's/\/$//g')
+outputFile="${outputFolder}/$outputFile"
+
+convert $inputFile \
+  -size "${width}x$height" xc:none \
+  -gravity SouthWest \
+    -pointsize $pointSizeLarge -fill "rgba(255,255,255,0.3)" -weight bold -annotate +20+20 "MAIS VÍDEOS E FOTOS\nHTTPS://WWW.MY-SITE.APP" \
+  $outputFile
+
+echo "Processado: $inputFile -> $outputFile"
